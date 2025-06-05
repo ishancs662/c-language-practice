9d540d7c2b5a9b83399b0e90923879cf22bdd1c0
Ishan Khan <khaneshan166@gmail.com>
2025-06-05 11:30:48 +0530

    Update and rename exam.c to intToBinary.c

--- intToBinary.c
+++ intToBinary.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 #include <stdint.h>
 
-// Function to print an integer in binary
 void printBinary(uint32_t number, int bits) {
     for (int i = bits - 1; i >= 0; i--) {
-        // Shift the bit to the rightmost position and mask with 1 to get the bit value
         printf("%d", (number >> i) & 1);
     }
     printf("\n");
 }
 
 int main() {
-    uint32_t number = 120; // Example integer
-    int bits = sizeof(number) * 8; // Number of bits in the integer type
+    uint32_t number = 120;
+    int bits = sizeof(number) * 8; 
 
     printf("Number in binary: ");
     printBinary(number, bits);
